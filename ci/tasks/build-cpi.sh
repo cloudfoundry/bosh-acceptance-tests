cf11cf4050e8cd2f169c7119be6501e225c3a435
caleb miles <cmiles@pivotal.io>
2015-04-30 10:21:24 -0700

    wip: bats
    Signed-off-by: Ted Young <tyoung@pivotal.io>

--- ci/tasks/build-cpi.sh
+++ ci/tasks/build-cpi.sh
@@ -9,4 +9,4 @@ echo "building CPI release..."
 pushd cpi-release
 bosh create release --name $CPI_RELEASE_NAME --version 0.0.0 --with-tarball
 popd
-mv cpi-release/dev_releases/cpi-release/$CPI_RELEASE_NAME/$CPI_RELEASE_NAME-0.0.0.tgz cpi-release.tgz
+mv cpi-release/dev_releases/$CPI_RELEASE_NAME/$CPI_RELEASE_NAME-0.0.0.tgz cpi-release.tgz
