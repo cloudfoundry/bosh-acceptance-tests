b7c3e12fd188a9bb9c0845234910f0e0e14d1928
caleb miles <cmiles@pivotal.io>
2015-04-30 10:04:27 -0700

    WIP: Try and use bosh-cli from rubygems
    Signed-off-by: Ted Young <tyoung@pivotal.io>

--- ci/tasks/build-cpi.sh
+++ ci/tasks/build-cpi.sh
@@ -2,17 +2,18 @@
 
 set -e -x
 
-# echo "installing BOSH CLI"
-# gem install bosh_cli --no-ri --no-rdoc
+echo "installing BOSH CLI"
+gem install bosh_cli --no-ri --no-rdoc
 
 echo "building CPI release..."
 export BUNDLE_GEMFILE=$PWD/bosh-src/Gemfile
-echo "installing bundler"
-gem install bundler --no-ri --no-rdoc
-bundle install
+
+#echo "installing bundler"
+#gem install bundler --no-ri --no-rdoc
+#bundle install
 
 pushd cpi-release
-bundle exec bosh create release \
-# bosh create release --name $CPI_RELEASE_NAME --version 0.0.0 --with-tarball
+#bundle exec bosh create release \
+bosh create release --name $CPI_RELEASE_NAME --version 0.0.0 --with-tarball
 popd
 mv cpi-release/dev_releases/cpi-release/$CPI_RELEASE_NAME-0.0.0.tgz cpi-release.tgz
