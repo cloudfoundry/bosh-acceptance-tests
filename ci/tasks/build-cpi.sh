14efcc31717d438d780e0e59252c4f36f2cb9a34
caleb miles <cmiles@pivotal.io>
2015-04-30 16:43:06 -0700

    WIP: bats
    Signed-off-by: Ted Young <tyoung@pivotal.io>

--- ci/tasks/build-cpi.sh
+++ ci/tasks/build-cpi.sh
@@ -2,11 +2,8 @@
 
 set -e -x
 
-echo "installing BOSH CLI"
-gem install bosh_cli --no-ri --no-rdoc
-
 echo "building CPI release..."
 pushd cpi-release
-bosh create release --name $CPI_RELEASE_NAME --version 0.0.0 --with-tarball
+bosh create release --name $cpi_release_name --version 0.0.0 --with-tarball
 popd
-mv cpi-release/dev_releases/$CPI_RELEASE_NAME/$CPI_RELEASE_NAME-0.0.0.tgz $CPI_RELEASE_NAME.tgz
+mv cpi-release/dev_releases/$cpi_release_name/$cpi_release_neame-0.0.0.tgz $cpi_release_name.tgz
