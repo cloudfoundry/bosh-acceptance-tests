aab2f31a43c3c0c8e305348d7490bba80c059a15
Jonathan Barnes <jbarnes@pivotal.io>
2015-05-05 17:21:19 -0700

    The BATs config is now generated in the run_bats
    It is no longer pulled from deployments bosh because needs to get the
    current directors UUID and thus is no longer static.
    
    We have also changed the place that defines the BAT_DEPLOYMENT_SPEC
    environment variable.  It no longer needs to be saved in the secrets
    file and pulled from the deployments bosh repo which should help with
    readability.  All other environment variables needed by BATs still
    follows the pattern of coming out of the secrets.
    
    [#73410854]

--- ci/tasks/run-bats.sh
+++ ci/tasks/run-bats.sh
@@ -6,6 +6,34 @@ eval $(ssh-agent)
 chmod go-r bosh-deployments/keys/bosh-dev.key
 ssh-add bosh-deployments/keys/bosh-dev.key
 
+bosh target $BAT_DIRECTOR
+
+export BAT_DEPLOYMENT_SPEC=$PWD/bats_config.yml
+cat << EOF > $BAT_DEPLOYMENT_SPEC
+---
+cpi: aws
+properties:
+  vip: 52.0.147.194
+  second_static_ip: 10.10.0.30
+  uuid: $(bosh status --uuid)
+  pool_size: 1
+  stemcell:
+    name: bosh-aws-xen-ubuntu-trusty-go_agent
+    version: latest
+  instances: 1
+  key_name:  bosh-dev
+  networks:
+    - name: default
+      static_ip: 10.10.0.29
+      type: manual
+      cidr: 10.10.0.0/24
+      reserved: [10.10.0.2-10.10.0.9]
+      static: [10.10.0.10-10.10.0.30]
+      gateway: 10.10.0.1
+      subnet: subnet-015e6d47
+      security_groups: [bat]
+EOF
+
 cd bats
 bundle install
 bundle exec rspec spec
