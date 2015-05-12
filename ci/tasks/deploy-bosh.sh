2ed64cb2b2dbfed926c846ba89e94b19835be728
Jonathan Barnes <jbarnes@pivotal.io>
2015-05-11 17:07:00 -0700

    Create tmp folder before normalizing dev cpi release
    [#93133456]

--- ci/tasks/deploy-bosh.sh
+++ ci/tasks/deploy-bosh.sh
@@ -5,6 +5,7 @@ set -e -x
 semver=`cat version-semver/number`
 
 echo "normalizing cpi release filename to value referenced in $manifest_path"
+mkdir ./tmp
 mv bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
 
 initver=$(cat bosh-init/version)
