37b08af34141f4690d614f711c366b7213fa9f7d
Jonathan Barnes <jbarnes@pivotal.io>
2015-05-14 14:34:01 -0700

    Removes 'checkout master' call
    This should not know what branch it should be working against, instead
    it should commit to whatever branch it given it by the ci resource
    [#93133456]
    Signed-off-by: Xing Zhou <xingzhou@cn.ibm.com>

--- ci/tasks/save-deployment-state.sh
+++ ci/tasks/save-deployment-state.sh
@@ -6,7 +6,6 @@ manifest_dir=bosh-deployments/concourse/$cpi_release_name
 
 echo "checking in BOSH deployment state"
 cd deploy-bosh/$manifest_dir
-git checkout master
 git add director-manifest-state.json
 git config --global user.email "cf-bosh-eng+bosh-ci@pivotal.io"
 git config --global user.name "bosh-ci"
