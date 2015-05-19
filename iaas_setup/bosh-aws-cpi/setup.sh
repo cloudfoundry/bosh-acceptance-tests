d262efad8761715e85e6f56f17e545edf79ef8ef
Aaron Hurley <ahurley@pivotal.io>
2015-05-19 15:56:17 -0700

    Update key names
    Signed-off-by: Colin Obyrne <cobyrne@pivotal.io>

--- iaas_setup/bosh-aws-cpi/setup.sh
+++ iaas_setup/bosh-aws-cpi/setup.sh
@@ -12,8 +12,8 @@ wget https://dl.bintray.com/mitchellh/terraform/terraform_0.5.1_linux_amd64.zip
 unzip terraform*.zip
 
 ./terraform plan -out=bats-dependencies.tfplan \
-  -var "access_key=$access_key_id" \
-  -var "secret_key=$secret_access_key" \
+  -var "access_key=$aws_access_key_id" \
+  -var "secret_key=$aws_secret_access_key" \
   -var "build_id=bats-$version" \
   -var "concourse_ip=52.4.241.132" \
   $terraform_dir
