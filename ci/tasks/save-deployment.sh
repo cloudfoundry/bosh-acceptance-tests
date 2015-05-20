8ac039ade19161ac85449970f9fba9b31ae0a7c2
Xing Zhou <xingzhou@cn.ibm.com>
2015-05-20 15:25:24 -0700

    Fix resource paths.
    [#95088852](https://www.pivotaltracker.com/story/show/95088852)
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/tasks/save-deployment.sh
+++ ci/tasks/save-deployment.sh
@@ -5,7 +5,7 @@ set -e -x
 manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
 
 echo "checking in BOSH deployment state"
-cd deploy-bosh/$manifest_dir
+cd deploy/$manifest_dir
 git add $base_os-director-manifest-state.json
 git config --global user.email "cf-bosh-eng+bosh-ci@pivotal.io"
 git config --global user.name "bosh-ci"
