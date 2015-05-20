b397962653a652c35360e8e85261ad98bb13d6ec
Xing Zhou <xingzhou@cn.ibm.com>
2015-05-20 10:53:45 -0700

    Rework AWS BATS task config and deployment manifest.
    The idea here is to have a more generic deployment manifest, with all
    dependencies in "$PWD/tmp".
    
    [#94045016]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/tasks/deploy-bosh.sh
+++ ci/tasks/deploy-bosh.sh
@@ -6,17 +6,19 @@ source /etc/profile.d/chruby.sh
 chruby 2.1.2
 
 semver=`cat version-semver/number`
+manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
+manifest_filename=$manifest_dir/$base_os-director-manifest.yml
 
-echo "normalizing cpi release filename to value referenced in $manifest_path"
+echo "normalizing paths to match values referenced in $manifest_filename"
 mkdir ./tmp
-mv bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
+mv ./bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
+mv ./bosh-release/release.tgz ./tmp/bosh-release.tgz
+mv ./stemcell/stemcell.tgz ./tmp/stemcell.tgz
+cp ./bosh-concourse-ci/pipelines/bosh-aws-cpi/bats.pem ./tmp/bats.pem
 
 initver=$(cat bosh-init/version)
 initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
 chmod +x $initexe
-
-manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
-manifest_filename=$manifest_dir/$base_os-director-manifest.yml
 
 echo "deleting existing BOSH Director VM..."
 $initexe delete $manifest_filename
