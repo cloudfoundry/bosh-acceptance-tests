b638afc6fecdd719de89e22cc9a0a18e490d9160
Aaron Hurley <ahurley@pivotal.io>
2015-05-22 09:55:39 -0700

    Change to use $cpi_release_name to make the script be more generic
    [#73636452]
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -14,7 +14,7 @@ mkdir ./tmp
 mv ./bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
 mv ./bosh-release/release.tgz ./tmp/bosh-release.tgz
 mv ./stemcell/stemcell.tgz ./tmp/stemcell.tgz
-cp ./bosh-concourse-ci/pipelines/bosh-aws-cpi/bats.pem ./tmp/bats.pem
+cp ./bosh-concourse-ci/pipelines/$cpi_release_name/bats.pem ./tmp/bats.pem
 
 initver=$(cat bosh-init/version)
 initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
