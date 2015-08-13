3ca9bf59f92cde467aa2cd193f746bbc0b8c026b
Abel Hu <abelch@microsoft.com>
2015-08-13 10:48:53 +0800

    Add Azure support

--- lib/bat/bosh_helper.rb
+++ lib/bat/bosh_helper.rb
@@ -41,6 +41,10 @@ module Bat
       @env.bat_infrastructure == 'warden'
     end
 
+    def azure?
+      @env.bat_infrastructure == 'azure'
+    end
+
     def compiled_package_cache?
       info = @bosh_api.info
       info['features'] && info['features']['compiled_package_cache']
