f853d353071c5a01df5fd89549c2084e4e9fd6a5
Luke Woydziak <luke.woydziak@emc.com>
2016-01-12 14:07:37 -0800

    Revert "Merge branch 'master' into develop"
    This reverts commit 6d0e27772aaec73b58b87129d9585307d395e539, reversing
    changes made to 2167c6291dadb9cef8e9724f57f4cd11d2680bd3.

--- spec/system/with_release_stemcell_deployment_spec.rb
+++ spec/system/with_release_stemcell_deployment_spec.rb
@@ -63,19 +63,4 @@ describe 'with release, stemcell and deployment', core: true do
       end
     end
   end
-
-  describe 'restore' do
-    it 'should restore director DB' do
-      with_tmpdir do
-        expect(bosh_safe('backup one_deployment.tgz')).to succeed_with /Backup of BOSH director was put in.*one_deployment\.tgz/
-        expect(bosh_safe("delete deployment #{deployment_name}")).to succeed_with /Deleted deployment/
-        expect(bosh_safe('backup no_deployment.tgz')).to succeed_with /Backup of BOSH director was put in.*no_deployment\.tgz/
-        expect(bosh_safe('restore one_deployment.tgz')).to succeed_with /Restore done!/
-        expect(bosh_safe('deployments')).to succeed_with /#{deployment_name}/
-        expect(bosh_safe('restore no_deployment.tgz')).to succeed_with /Restore done!/
-        result = bosh_safe('deployments')
-        expect(result.output).to match_regex(/No deployments/)
-      end
-    end
-  end
 end
