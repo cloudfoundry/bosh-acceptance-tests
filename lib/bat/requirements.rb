428b8b2731059f099b3f20c649ec0912e9c1d959
Tom Viehman <tviehman@pivotal.io>
2017-02-15 15:59:44 -0800

    Fixes upload-release call to succesfully locate release directory
    [#125440001](https://www.pivotaltracker.com/story/show/125440001)
    
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- lib/bat/requirements.rb
+++ lib/bat/requirements.rb
@@ -90,7 +90,7 @@ module Bat
         @logger.info('release already uploaded')
       else
         @logger.info('release not uploaded')
-        expect(@bosh_runner.bosh_safe("upload-release #{what.to_path}")).to succeed
+        expect(@bosh_runner.bosh_safe("upload-release --dir #{what.path} #{what.to_path}")).to succeed
       end
     end
 
