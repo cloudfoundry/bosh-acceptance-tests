68e14028f7de529b79369409e9ad0eb75f2d7558
Dennis Leon <dleon@pivotal.io>
2017-03-03 14:26:00 -0800

    Generate a private_key and use it when deploying director
    [#125440001](https://www.pivotaltracker.com/story/show/125440001)
    
    Signed-off-by: J Evelyn Quintana <jquintana@pivotal.io>

--- ci/tasks/deploy-director.sh
+++ ci/tasks/deploy-director.sh
@@ -28,12 +28,14 @@ EOF
 export bosh_cli=$(realpath bosh-cli/bosh-cli-*)
 chmod +x $bosh_cli
 
+$bosh_cli interpolate bats/ci/assets/ssh_key.yml --vars-store=/tmp/ssh_keystore
+
 $bosh_cli interpolate bosh-deployment/bosh.yml \
   -o bosh-deployment/aws/cpi.yml \
   --vars-store director-creds.yml \
   -v director_name=bats-director \
-  --ops-file bats/ci/assets/local-aws-cpi-release.yml \
-  --ops-file bats/ci/assets/ssh_key.yml \
+  -v private_key=$($bosh_cli interpolate /tmp/ssh_keystore --path=/private_key/private_key) \
+  -o bats/ci/assets/local-aws-cpi-release.yml \
   --vars-env "BOSH" > director.yml
 
 $bosh_cli create-env director.yml -l director-creds.yml
