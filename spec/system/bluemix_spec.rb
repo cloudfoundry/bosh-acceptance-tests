bde39723c2a13bf526222c26d54f58468d719b61
zhangtbj <zhangtbj@cn.ibm.com>
2017-06-30 17:02:28 +0800

    add bluemix vcap test

---
+++ spec/system/bluemix_spec.rb
@@ -0,0 +1,25 @@
+require 'system/spec_helper'
+
+describe 'check vcap password correct', type: 'os' do
+  before(:all) do
+    @requirements.requirement(@requirements.stemcell)
+    @requirements.requirement(@requirements.release)
+    load_deployment_spec
+    @requirements.requirement(deployment, @spec)
+  end
+
+  after(:all) do
+    @requirements.cleanup(deployment)
+  end
+
+  fit 'vcap should exist in shadow and user_data.json' do
+    passwd = '$6$3n/Y5RP0$Jr1nLxatojY9Wlqduzwh66w8KmYxjoj9vzI62n3Mmstd5mNVnm0SS1N0YizKOTlJCY5R/DFmeWgbkrqHIMGd51'
+    output_shadow = bosh_ssh('batlight', 0, "cat /etc/shadow | grep #{passwd}").output
+    running_services_shadow = output_shadow.split("\n").uniq
+    expect(running_services_shadow).to include(passwd)
+
+    output_json = bosh_ssh('batlight', 0, "cat /var/vcap/bosh/user_data.json | grep #{passwd}").output
+    running_services_json = output_json.split("\n").uniq
+    expect(running_services_json).to include(passwd)
+  end
+end
