a351e520736560de06472b8beeb02b942ecb01b0
zhangtbj <zhangtbj@cn.ibm.com>
2017-07-03 22:20:42 +0800

    Update bluemix_spec.rb

--- spec/system/bluemix_spec.rb
+++ spec/system/bluemix_spec.rb
@@ -14,11 +14,11 @@ describe 'check vcap password correct 222' do
 
   fit 'vcap should exist in shadow and user_data.json' do
     passwd = '$6$3n/Y5RP0$Jr1nLxatojY9Wlqduzwh66w8KmYxjoj9vzI62n3Mmstd5mNVnm0SS1N0YizKOTlJCY5R/DFmeWgbkrqHIMGd51'
-    output_shadow = bosh_ssh('batlight', 0, "sudo cat /etc/shadow | grep #{passwd}").output
+    output_shadow = bosh_ssh('batlight', 0, "sudo cat /etc/shadow").output
     running_services_shadow = output_shadow.split("\n").uniq
     expect(running_services_shadow).to include(passwd)
 
-    output_json = bosh_ssh('batlight', 0, "sudo cat /var/vcap/bosh/user_data.json | grep #{passwd}").output
+    output_json = bosh_ssh('batlight', 0, "sudo cat /var/vcap/bosh/user_data.json").output
     running_services_json = output_json.split("\n").uniq
     expect(running_services_json).to include(passwd)
   end
