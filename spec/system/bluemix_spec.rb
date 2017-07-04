7b395cd83fc6dd8654e58db24e578d32b9aded8e
zhangtbj <zhangtbj@cn.ibm.com>
2017-07-04 10:09:59 +0800

    refine vcap test4

--- spec/system/bluemix_spec.rb
+++ spec/system/bluemix_spec.rb
@@ -1,6 +1,6 @@
 require 'system/spec_helper'
 
-describe 'check vcap password correct 222' do
+describe 'check vcap password correct' do
   before(:all) do
     @requirements.requirement(@requirements.stemcell)
     @requirements.requirement(@requirements.release)
@@ -13,15 +13,15 @@ describe 'check vcap password correct 222' do
   end
 
   fit 'vcap should exist in shadow and user_data.json' do
-    passwd = '$6$3n/Y5RP0$Jr1nLxatojY9Wlqduzwh66w8KmYxjoj9vzI62n3Mmstd5mNVnm0SS1N0YizKOTlJCY5R/DFmeWgbkrqHIMGd51'
+    passwd = ["$6$3n/Y5RP0$Jr1nLxatojY9Wlqduzwh66w8KmYxjoj9vzI62n3Mmstd5mNVnm0SS1N0YizKOTlJCY5R/DFmeWgbkrqHIMGd51"]
 
     output_json = bosh_ssh('batlight', 0, "sudo cat /var/vcap/bosh/user_data.json").output
-    #running_services_json = output_json.split("\n").uniq
-    expect(output_json).to include(passwd)
+    running_services_json = output_json.split("\n").uniq
+    expect(running_services_json).to include(*passwd)
 
     output_shadow = bosh_ssh('batlight', 0, "sudo cat /etc/shadow").output
-    #running_services_shadow = output_shadow.split("\n").uniq
-    expect(output_shadow).to include(passwd)
+    running_services_shadow = output_shadow.split("\n").uniq
+    expect(running_services_shadow).to include(*passwd)
 
   end
 end
