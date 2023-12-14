c7ebe1ef23953583eeaa2bc1bedaf7e5566238fb
Aram Price <pricear@vmware.com>
2023-12-14 15:51:37 -0800

    Replace the OTHER "type: os" with the "os: true"
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- spec/system/auditd_spec.rb
+++ spec/system/auditd_spec.rb
@@ -1,6 +1,6 @@
 require 'system/spec_helper'
 
-describe 'auditd, sshd, cron, rsyslogd', type: 'os' do
+describe 'auditd, sshd, cron, rsyslogd', os: true do
   before(:each) do
     @requirements.requirement(@requirements.stemcell)
     @requirements.requirement(@requirements.release)
