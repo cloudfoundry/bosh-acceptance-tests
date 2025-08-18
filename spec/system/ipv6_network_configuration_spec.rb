43913cf87a90edc81cbba81861ecf73cab6cad77
neddp <nedd.petrov@gmail.com>
2025-08-18 15:24:25 +0300

    Clean up the before and after blocks. Keep one deployment for all tests.

--- spec/system/ipv6_network_configuration_spec.rb
+++ spec/system/ipv6_network_configuration_spec.rb
@@ -6,55 +6,45 @@ describe 'IPv6 network configuration', ipv6: true do
   before(:all) do
     @requirements.requirement(@requirements.stemcell)
     @requirements.requirement(@requirements.release)
-  end
-
-  before(:all) do
     load_deployment_spec
     use_static_ip
     use_vip
-    @requirements.requirement(deployment, @spec)
+    
+    @deployment = use_multiple_manual_networks
+    @deployment = with_deployment
   end
 
   after(:all) do
-    @requirements.cleanup(deployment)
+    @requirements.cleanup(@deployment) if @deployment
   end
 
   context 'when using manual networking and ipv6', ipv6_manual_networking: true do
-    let(:deployment) do
-      use_multiple_manual_networks
-      with_deployment
+    before(:all) do
+      @requirements.requirement(@deployment, @spec)
     end
 
     it 'supports manual network dual stack on AWS', ipv6_dual_stack: true, ssh: true do
-      expect(bosh("-d #{deployment.name} deploy #{deployment.to_path}")).to succeed
-
-      output = bosh_ssh('batlight', 0, 'PATH=/sbin:/usr/sbin:$PATH; ifconfig', deployment: deployment.name).output
+      output = bosh_ssh('batlight', 0, 'PATH=/sbin:/usr/sbin:$PATH; ifconfig', deployment: @deployment.name).output
       expect(output).to include(static_ips[0])
       expect(output).to include(static_ips[1])
     end
   end
 
   context 'when allocating IPv6 prefix', ipv6_prefix_allocation: true do
-    let(:deployment) do
-      use_multiple_manual_networks
-      with_deployment
+    before(:all) do
+      @requirements.requirement(@deployment, @spec)
     end
-
-    before do
-      expect(bosh("-d #{deployment.name} deploy #{deployment.to_path}")).to succeed
-    end
-
 
     it 'returns the expected IPv6 prefix from AWS metadata', ssh: true do
-      _, _, prefix = fetch_ipv6_prefix_from_aws_metadata('batlight', 0, deployment.name)
+      _, _, prefix = fetch_ipv6_prefix_from_aws_metadata('batlight', 0, @deployment.name)
       expect(prefix).to eq('80')
     end
 
     it 'verifies the IPv6 prefix in spec.json', ssh: true do
-      _, ip, prefix = fetch_ipv6_prefix_from_aws_metadata('batlight', 0, deployment.name)
+      _, ip, prefix = fetch_ipv6_prefix_from_aws_metadata('batlight', 0, @deployment.name)
 
       cli_cmd = 'output_data=$(sudo cat /var/vcap/bosh/spec.json); echo "----$output_data----"'
-      spec_output = bosh_ssh('batlight', 0, cli_cmd, deployment: deployment.name).output
+      spec_output = bosh_ssh('batlight', 0, cli_cmd, deployment: @deployment.name).output
 
       json_str = extract_ssh_stdout_between_dashes(spec_output)
       spec = JSON.parse(json_str)
