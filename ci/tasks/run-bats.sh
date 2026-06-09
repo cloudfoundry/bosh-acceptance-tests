3ebeafed6f89d4978df8bf1a7c8cb44a64f69799
aram price <aram.price@broadcom.com>
2026-06-09 15:30:47 -0700

    CI: cleanup `run-bats` task
    - remove `image_resource`
      - this pointed at an outdated location; should be provided by the
        invoker
    - shellcheck fixes

--- ci/tasks/run-bats.sh
+++ ci/tasks/run-bats.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-set -e
-
-export BAT_STEMCELL=$(realpath stemcell/*.tgz)
-export BAT_DEPLOYMENT_SPEC=$(realpath bats-config/bats-config.yml)
-export BAT_BOSH_CLI=$(realpath "$(which bosh)")
+BAT_STEMCELL=$(realpath stemcell/*.tgz)
+BAT_DEPLOYMENT_SPEC=$(realpath bats-config/bats-config.yml)
+BAT_BOSH_CLI=$(realpath "$(command -v bosh)")
+export BAT_STEMCELL
+export BAT_DEPLOYMENT_SPEC
+export BAT_BOSH_CLI
 
 source bats-config/bats.env
 
-pushd $(realpath bats)
+pushd "$(realpath bats)"
   bundle install
-  bundle exec rspec spec $BAT_RSPEC_FLAGS
+
+  if declare -p BAT_RSPEC_FLAGS 2>/dev/null | grep -q 'declare \-a'; then
+    bundle exec rspec spec "${BAT_RSPEC_FLAGS[@]}"
+  else
+    # shellcheck disable=SC2086
+    bundle exec rspec spec ${BAT_RSPEC_FLAGS:-}
+  fi
 popd
